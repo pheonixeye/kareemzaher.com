3c289fd2631e2e9d86bf61ba4cab69214fa60007
pheonixeye <drkazgm@gmail.com>
2023-07-04 19:16:08 +0300

    responsive & navigation overlay done

---
+++ navbar.js
@@ -0,0 +1,11 @@
+const overlayNav = document.querySelector(".overlay-nav");
+
+function openNav() {
+  overlayNav.style.width = "50%";
+}
+
+function closeNav() {
+  overlayNav.style.width = "0%";
+}
+
+//TODO: add event listener to close overlay if user taps anywhere on the screen
