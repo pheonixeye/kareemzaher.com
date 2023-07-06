584259b95d4b8eea752d150d85a57742ff280650
pheonixeye <drkazgm@gmail.com>
2023-07-06 05:55:46 +0300

    dynamic calender nearly complete

--- js/flip_calender.js
+++ js/flip_calender.js
@@ -19,6 +19,16 @@ const weekDays = {
   6: { a: "Saturday", b: "SAT" },
 };
 
+const initialToint = {
+  SUN: 0,
+  MON: 1,
+  TUE: 2,
+  WED: 3,
+  THU: 4,
+  FRI: 5,
+  SAT: 6,
+};
+
 const months = {
   0: { a: "January", b: "JAN", d: 31 },
   1: { a: "February", b: "FEB", d: isLeapYear ? 29 : 28 },
@@ -75,24 +85,30 @@ for (let i = 0; i < Object.keys(months).length; i++) {
 }
 
 const thisMonthListOfDays = dateToDay[date.getMonth()];
+const nextMonthListOfDays =
+  dateToDay[date.getMonth() + 1 === 12 ? 0 : date.getMonth() + 1];
 const firstDayofThisMonth = dateToDay[date.getMonth()][0];
-
-// console.log(dateToDay);
-// console.log(thisMonthListOfDays);
-console.log(firstDayofThisMonth);
 
 const weekdayDay = document.getElementById("calender-header-weekday-day");
 const monthYear = document.getElementById("calender-header-month-year");
 
-//set date of calender header
+//set day/date of calender header
 weekdayDay.innerHTML = `${weekDays[date.getDay()].a} ${date.getDate()}${buildTH(
   date.getDate()
 )}`;
 
+//set month/year of calender header
 monthYear.innerHTML = `${months[date.getMonth()].a} ${date.getFullYear()}`;
 
-let currentMonth = document.querySelector("#current-month");
+//function to add or subtract months
 
+function moveMonth() {
+  //TODO:
+}
+
+const currentMonth = document.querySelector("#current-month");
+
+//set row of weekdays
 for (let index = 0; index < Object.keys(weekDays).length; index++) {
   currentMonth.insertAdjacentHTML(
     "beforeend",
@@ -103,22 +119,115 @@ for (let index = 0; index < Object.keys(weekDays).length; index++) {
   );
 }
 
-for (let index = 0; index < thisMonthListOfDays.length; index++) {
-  const firstWeekdayItem = document.querySelector(
-    `#${firstDayofThisMonth["weekdayInitials"]}`
-  );
-  //   console.log(thisMonthListOfDays[index]);
-  //   console.log(firstWeekdayItem);
-  if (firstWeekdayItem.id === thisMonthListOfDays[index]["weekdayInitials"]) {
-    const weekdayInt = index + 1;
-    console.log(weekdayInt);
-  } else {
+//grid item counter
+var itemCounter = 0;
+
+function buildPreviousMonthDays() {
+  const previousMonthDays =
+    dateToDay[date.getMonth() - 1 === -1 ? 11 : date.getMonth() - 1];
+
+  const previousMonthDaysLength = previousMonthDays.length;
+
+  const skipCount = initialToint[firstDayofThisMonth["weekdayInitials"]];
+
+  const startingDayFromPreviousMonth = previousMonthDaysLength - skipCount;
+
+  for (let index = 1; index <= skipCount; index++) {
     currentMonth.insertAdjacentHTML(
       "beforeend",
-      ` <span id="${thisMonthListOfDays[index].date}" class="calender-day-item wd-${thisMonthListOfDays[index].weekday}">
-              ${thisMonthListOfDays[index].date}
-              </span>
-              `
+      ` <span id="${
+        startingDayFromPreviousMonth + index
+      }" class="calender-day-item not-available">
+           ${startingDayFromPreviousMonth + index}
+           </span>
+           `
+    );
+    itemCounter++;
+  }
+}
+
+function helperNormalizeNumbersView(number) {
+  number = number.toString();
+  if (number.length < 2) {
+    return `0${number}`;
+  } else {
+    return number.toString();
+  }
+}
+
+buildPreviousMonthDays();
+
+function buildCurrentMonthDays() {
+  for (let index = 0; index < thisMonthListOfDays.length; index++) {
+    const notAvailable = thisMonthListOfDays[index].date < date.getDate();
+    const isToday = thisMonthListOfDays[index].date === date.getDate();
+    currentMonth.insertAdjacentHTML(
+      "beforeend",
+      ` <span id="${
+        thisMonthListOfDays[index].date
+      }" class="calender-day-item wd-${thisMonthListOfDays[index].weekday} ${
+        notAvailable ? "not-available" : ""
+      } ${isToday ? "is-today" : ""}">
+         ${helperNormalizeNumbersView(thisMonthListOfDays[index].date)}
+         </span>
+         `
+    );
+    itemCounter++;
+  }
+}
+
+buildCurrentMonthDays();
+
+function buildNextMonthDays() {
+  for (let index = 0; index < 42 - itemCounter; index++) {
+    currentMonth.insertAdjacentHTML(
+      "beforeend",
+      ` <span id="${nextMonthListOfDays[index].date}"
+       class="calender-day-item wd-${nextMonthListOfDays[index].weekday}">
+      ${helperNormalizeNumbersView(nextMonthListOfDays[index].date)}
+      </span>
+               `
     );
   }
 }
+
+buildNextMonthDays();
+
+function activateCalenderItem() {
+  const items = document.querySelectorAll(".calender-day-item");
+  items.forEach((e) => {
+    if (!e.classList.contains("not-available")) {
+      e.addEventListener("click", (event) => {
+        items.forEach((e) => {
+          e.classList.remove("is-active");
+        });
+        e.classList.toggle("is-active");
+      });
+    }
+  });
+}
+
+activateCalenderItem();
+
+function animateFrontToBack() {
+  const calender = document.querySelector(".calender");
+  const front = document.querySelector(".calender-front");
+  const back = document.querySelector(".calender-back");
+  const items = document.querySelectorAll(".calender-day-item");
+  items.forEach((e) => {
+    e.addEventListener("click", (event) => {
+      calender.classList.add("flip");
+      front.style.display = "none";
+      back.classList.remove("flip");
+      back.style.display = "";
+    });
+  });
+  back.addEventListener("click", (event) => {
+    calender.classList.remove("flip");
+    front.style.display = "";
+    back.classList.add("flip");
+    back.style.display = "none";
+  });
+}
+
+animateFrontToBack();
