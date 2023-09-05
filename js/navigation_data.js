972142e0f98ae9b0a21ef0159a6923dc61523acc
pheonixeye <drkazgm@gmail.com>
2023-09-05 21:51:59 +0300

    fix: mobile nav links

--- js/navigation_data.js
+++ js/navigation_data.js
@@ -8,11 +8,11 @@ class NavData {
 
 const navData = [
   new NavData("Home", "./", "index-id"),
-  new NavData("Appointments", "appointments.html", "appointments-id"),
-  new NavData("Services", "services.html", "services-id"),
-  new NavData("Articles", "articles.html", "articles-id"),
-  new NavData("Reviews", "reviews.html", "reviews-id"),
-  new NavData("Erection-Test", "erection_test.html", "erection-test-id"),
+  new NavData("Appointments", "./appointments.html", "appointments-id"),
+  new NavData("Services", "./services.html", "services-id"),
+  new NavData("Articles", "./articles.html", "articles-id"),
+  new NavData("Reviews", "./reviews.html", "reviews-id"),
+  new NavData("Erection-Test", "./erection_test.html", "erection-test-id"),
 ];
 
 export default navData;
