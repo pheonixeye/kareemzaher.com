2b5607a0c4c30f7a54275551ca611ce4c6650637
pheonixeye <drkazgm@gmail.com>
2024-08-15 22:29:03 +0300

    added article 1 - pe

--- articles/articles_meta.js
+++ articles/articles_meta.js
@@ -7,4 +7,11 @@ export const articlesMeta = [
       imgUrl: "https://res.cloudinary.com/dw5zbj6pk/image/upload/v1695182078/o3z0zicd7dj9kwzshgmx.webp",
       filePath: "articles/article0.html"
    },
+   {
+      id: 1,
+      title: 'سرعة القذف عند الرجال: الأسباب والعلاج',
+      description: 'سرعة القذف تُعد من المشكلات الشائعة التي يواجهها الرجال، وتسبب الكثير من القلق والتوتر. يُعاني العديد من الرجال من هذه المشكلة في مرحلة ما من حياتهم، وهي من الأمور التي قد تؤثر سلباً على الحياة الزوجية. في هذا المقال، سنتعرف على الأسباب المحتملة لسرعة القذف، وكيفية التعامل معها بطرق سهلة ومفهومة.',
+      imgUrl: "https://res.cloudinary.com/dw5zbj6pk/image/upload/v1723749395/Premature-Ejaculation-800x480_bittax.webp",
+      filePath: "articles/article1.html"
+   },
 ];
